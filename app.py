ea2dd090c8b7b3e79f7101b4901523c1f6c8b0bb
Yagya Kulora <82093908+yagyak28@users.noreply.github.com>
2022-11-02 06:11:54 +0000

    first commit

---
+++ app.py
@@ -0,0 +1,42 @@
+from flask import Flask, render_template, jsonify
+
+app = Flask(__name__)
+
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Social Media Marketer',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Scientist',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Developer',
+    'location': 'Remote'
+  },
+  {
+    'id': 4,
+    'title': 'Software Development',
+    'location': 'San Francisco, USA',
+    'salary': '$150,000'
+  }
+]
+
+@app.route("/")
+def hello_people():
+    return render_template('home.html', 
+                           jobs=JOBS, 
+                           company_name='Kuloras-Careers')
+
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', debug=True)
